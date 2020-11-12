72352385d92e6c97d216fe1d6b31ecb462ba1f98
pkMyt1 <g2.checkpoint@gmail.com>
2020-11-11 19:15:36 -0500

    Fixing Commit Problems

--- docs/run_ScarMapper_Combine.sh
+++ docs/run_ScarMapper_Combine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Parameter file to run ScarMapper Combine module
-#File generated 13-Oct-2020
+#File generated 11-Nov-2020
 
 
 python3 /full/path/to/scarmapper.py --options_file /full/path/to/run_ScarMapper_Combine.sh
@@ -12,9 +12,8 @@ exit
 --SampleName	# Labels plot
 --DataFiles	/full/path/to/frequency/files/
 
-# Plot Options
---FigureType	pdf # svg, jpg, tiff, pdf
-
 --Verbose	INFO
 --Job_Name	# Labels output file
 
+# Plot Options
+--FigureType	pdf # svg, jpg, tiff, pdf, png
